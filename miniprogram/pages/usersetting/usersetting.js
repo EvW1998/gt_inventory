28b586632e17f285c50191ff1b80e07004e570d0
Evan <ew98@foxmail.com>
2019-12-23 14:38:47 +0800

    user with higher permission level can change others' name and level

--- miniprogram/pages/usersetting/usersetting.js
+++ miniprogram/pages/usersetting/usersetting.js
@@ -10,8 +10,10 @@ Page({
    */
   data: {
     manage_id: '',
-    manage_name: '',
-    manage_level: 0
+    manage_user: {},
+    max_level: 0,
+    filled_name: true,
+    btn_state: "default" // the state for the confirm button
   },
 
   /**
@@ -19,7 +21,8 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
-      manage_id: options.title
+      manage_id: options.title,
+      max_level: app.globalData.permission_level - 1
     })
 
     this.searchUser()
@@ -33,15 +36,108 @@ Page({
       })
       .get({
         success: res => {
-          console.log('Set for: ', res.data[0])
-
           this.setData({
-            manage_name: res.data[0].true_name,
-            manage_level: res.data[0].permission_level
+            manage_user: res.data[0]
           })
+
+          console.log('Set for: ', this.data.manage_user)
         }
       })
 
+  },
+
+  /**
+   *  check whether the real name val get filled
+   * 
+   *  par e: the value returned from the input text
+   */
+  checkBlur_name: function (e) {
+    if (e.detail.value != "") {
+      // if the name input text get filled with something
+      this.setData({
+        filled_name: true,
+        btn_state: "primary"
+      })
+    }
+    else {
+      // if the name input text get filled with nothing
+      this.setData({
+        filled_name: false,
+        btn_state: "default"
+      })
+    }
+  },
+
+  /**
+   *  When the confirm button triggered
+   * 
+   *  par e: the return val from the form submit
+   */
+  formSubmit: function (e) {
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
+
+    var update_user_data = {}
+
+    if(e.detail.value.name != this.data.manage_user.true_name) {
+      update_user_data['true_name'] = e.detail.value.name
+    }
+
+    if (e.detail.value.level != this.data.manage_user.permission_level) {
+      if(e.detail.value.level == '') {
+        update_user_data['permission_level'] = 0
+      }
+      else {
+        update_user_data['permission_level'] = e.detail.value.level
+      }
+    }
+
+    if(Object.keys(update_user_data).length != 0) {
+      // call dbAdd() cloud function to add the user to database user
+      wx.cloud.callFunction({
+        name: 'dbChangeUser',
+        data: {
+          collection_name: db_user,
+          update_data: update_user_data,
+          uid: this.data.manage_id
+        },
+        success: res => {
+          wx.hideLoading()
+          wx.showToast({
+            title: '更改成功',
+            duration: 1500,
+            complete: function (res) {
+              setTimeout(function () {
+                // take the user back to the info page
+                wx.navigateBack({
+                })
+              }, 1500)
+            }
+          })
+
+        },
+        fail: err => {
+          // if get a failed result
+          console.error('failed to use cloud function dbChangeUser()', err)
+        }
+      })
+    }
+    else {
+      wx.hideLoading()
+      wx.showToast({
+        title: '更改成功',
+        duration: 1500,
+        complete: function (res) {
+          setTimeout(function () {
+            // take the user back to the info page
+            wx.navigateBack({
+            })
+          }, 1500)
+        }
+      })
+    }
   },
 
   /***
